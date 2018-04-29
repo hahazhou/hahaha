906b939c739e97ee8d2c27689ad3d169753689eb
hahazhou <haha-zhou@sjtu.edu.cn>
2018-04-29 16:22:59 +0800

    Add files via upload

---
+++ demo1-2/src/main/java/com/example/demo/control/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.control;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+            .withUser("root")
+            .password("root")
+            .roles("USER");
+    }
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+      return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+}
+
